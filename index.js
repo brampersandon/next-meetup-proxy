10fb2359709a418d65806d51a5dc0f610473e229
Brandon Johnson <b@johnsonmn.org>
2018-12-14 14:42:17 -0600

    Factor out the 'MEETUP_NAME' env var

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const MEETUP_KEY = process.env.MEETUP_KEY
+if (!MEETUP_KEY) throw Error('Please provide a MEETUP_KEY variable')
+
+const MEETUP_NAME = process.env.MEETUP_NAME
+if (!MEETUP_NAME) throw Error('Please provide a MEETUP_NAME environment variable')
+
 const meetup = require('meetup-api')({ key: process.env.MEETUP_KEY })
 const cors = require('micro-cors')({
   origin: 'http://javascriptmn.com'
@@ -5,11 +11,12 @@ const cors = require('micro-cors')({
 
 exports.default = cors(async (req, res) => {
   return await (new Promise((resolve, reject) => {
-    meetup.getEvents({ 'group_urlname': 'javascriptmn' }, (err, events) => {
+    meetup.getEvents({ 'group_urlname': MEETUP_NAME }, (err, events) => {
       if (err) {
         reject(err)
         return
       }
+      // The 'zeroeth' event always has the latest! Sure hope it exists!
       resolve(events.results[0])
     })
   }))
